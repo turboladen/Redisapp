ccd02d7b974cc237a33c5f6b349f85a0067da03f
Steve Loveless <steve.loveless@gmail.com>
2014-01-22 22:24:17 -0800

    Initial barebones project

--- Redisapp/AppDelegate.rb
+++ Redisapp/AppDelegate.rb
@@ -7,10 +7,31 @@
 #
 
 class AppDelegate < OSX::NSObject
+  include OSX
 
-	def applicationDidFinishLaunching_(notification)
-		# Insert code here to initialize your application
-	end
+  def initialize
+    @redis_path = '/usr/local/bin/redis-server'
+    @redis_config = ' /usr/local/etc/redis.conf'
+  end
 
+  def applicationDidFinishLaunching_(notification)
+    statusbar = NSStatusBar.systemStatusBar
+    status_item = statusbar.statusItemWithLength(NSVariableStatusItemLength)
+    redis_icon = NSImage.imageNamed_('dbs-redis_24x24x32.png')
+    status_item.setImage(redis_icon)
+
+    @redis_pid = fork do
+      exec "#{@redis_path} #{@redis_config}"
+    end
+
+    puts "Forked process #{@redis_pid}"
+
+    Process.detach(@redis_pid)
+  end
+
+  def applicationWillTerminate_(notification)
+    puts "Killing redis process #{@redis_pid}"
+    Process.kill('INT', @redis_pid)
+  end
 end
 
